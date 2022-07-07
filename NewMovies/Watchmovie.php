ea5ada2ad9f8d92b9279708acc010aee542d205e
Vinhthai-0601 <80025976+Vinhthai-0601@users.noreply.github.com>
2022-07-08 00:09:06 +0700

    watchmovie
    version 1

---
+++ NewMovies/Watchmovie.php
@@ -0,0 +1,90 @@
+<?php 
+  include 'includes/header.php';
+?>
+<link rel="stylesheet" href="css/watch.css">
+<div class="watch">
+<video autoplay muted loop id="myVideo">
+        <source src="images/Marvel Studios' Thor_ Love and Thunder - Teaser Trailer (2022) Chris Hemsworth, Natalie Portman.mp4" type="video/mp4">
+</video>
+    <h1 style=" padding-left: 10px">Thor and Thunder</h1>
+    <h2 style="padding-left: 10px">Tóm tắt</h1>
+    <p style="padding-left: 10px">bla bla bla bla bla bla bla bla bla bla</p>
+    <span></span>
+    <div class="space" style="padding-top: 5rem;">
+
+</div>
+
+<div class="container-fluid pt-lg-5">
+  <div class="tile">
+    <h2 style="color: orange;" >Similar content:</h2>
+  </div>
+    <div id="carouselActionFlim" class="carousel slide" data-mdb-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="container-fluid" style="padding-right: 150px;">
+            <div class="row">
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <div class="container-fluid" style="padding-right: 150px;">
+            <div class="row">
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <div class="container-fluid" style="padding-right: 150px;">
+            <div class="row">
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+            <div class="col-md-3">
+              <img class="img-thumbnail" src="images/thor-love-and-thunder-poster.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <button class="carousel-control-next" type="button" data-mdb-target="#carouselActionFlim" data-mdb-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+</div>
+</div>
+
+
+<?php 
+  include 'includes/footer.php';
+?>
