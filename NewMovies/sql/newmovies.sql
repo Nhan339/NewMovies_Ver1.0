066129587b799e0a4e57bf9c8ef4ee126dd15550
coftbred <80310115+coftbred@users.noreply.github.com>
2022-07-19 21:24:14 +0700

    Update newmovies.sql

--- NewMovies/sql/newmovies.sql
+++ NewMovies/sql/newmovies.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 13, 2022 at 03:24 AM
--- Server version: 5.7.31
--- PHP Version: 7.4.9
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th7 19, 2022 lúc 02:23 PM
+-- Phiên bản máy phục vụ: 5.7.31
+-- Phiên bản PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `newmovies`
+-- Cơ sở dữ liệu: `newmovies`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -35,7 +35,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   `hash` varchar(255) NOT NULL,
   `user_role` int(10) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=244 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=246 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`ID`, `user_name`, `email`, `hash`, `user_role`) VALUES
+(244, 'NKLuyen', 'nkluyen123@gmail.com', '$2y$10$nXU2NtcmwOeNQsEEkA4Yk.YCCUdrvI8s3m8ltDADEpuxaenjBu.ny', 3),
+(245, 'Coftbred', 'manafer@gmail.com', '$2y$10$4IATz/zhX5rMfHDJ.vQ6tOmCpXSITuR56tOEtxLQzAUVWEhA7hQO.', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `video_url` varchar(255) NOT NULL,
+  `video_details` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
